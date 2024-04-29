02baf230642bfb8e8dca71f7c95f06307356a447
Zully <zullyortiz.09@gmail.com>
2024-04-29 12:25:18 -0500

    prueba

---
+++ menu.py
@@ -0,0 +1,85 @@
+def menuCamper():
+    print("""
+    /////////////////////////////////////////////////
+    ------------ BIENVENIDO CAMPER-------------------
+          1). Reportes
+          2). Cambiar usuarios y contraseñas.
+          3). Cambiar informacion.
+    -------------------------------------------------
+    /////////////////////////////////////////////////
+""")
+
+menuCamper()
+
+def menuCamperOpc1():
+    print("""
+    ---------------------REPORTES---------------------
+          1). Mostrar datos del Camper.
+          2). Riesgo del camper.
+          3). Ruta del camper.
+          4). Trainer del camper
+          5). Modulo en el que se encuetra el camper.
+    --------------------------------------------------
+""")
+menuCamperOpc1()
+
+def menuCamperOpc3():
+    print("""
+    ----------------- Cambiar información ------------------
+          1). Cambiar dirección.
+          2). Cambiar telefono movil.
+          3). Cambiar telefono fijo.
+    --------------------------------------------------------
+""")
+menuCamperOpc3()
+
+def menuTrainer():
+    print("""
+    ///////////////////////////////////////////////////
+    ----------- BIENVENIDO TRAINER --------------------
+          1). Cambiar usuario y contraseña
+          2). Ruta de entrenamiento.
+          3). Cambiar información.
+    ---------------------------------------------------
+    ///////////////////////////////////////////////////
+""")
+menuTrainer()
+
+def munuTrainerOpc3():
+    print("""
+    ----------------- Cambiar información ---------------
+          1). Cambiar dirección.
+          2). Cambiar telefono movil.
+          3). Cambiar telefono fijo 
+    -----------------------------------------------------
+""")
+menuCamperOpc3()
+
+def menuCoordinador():
+    print("""
+    //////////////////////////////////////////////////////////////////////
+    -------------------- BIENVENIDO COORDINADOR --------------------------
+          1). Cambiar usuarios y contraseñas de todo el sistema educativo.
+          2). Cambiar informacion de todo el sistema educativo.
+          3). Agregar nota de examen de aprobación.
+          4). Agregar nota de filtro. 
+          5). Consultar cuales campers se encuentra en riego.
+          6). Reporte.
+    -----------------------------------------------------------------------
+    ///////////////////////////////////////////////////////////////////////
+""")
+menuCoordinador()
+
+def menuCoordinadorOpc6():
+    print("""
+    ---------------------------------    Reporte   ----------------------------------------------------
+          1). Listar los campers que se encuentre en estado inscrito.
+          2). Listar los campers que aprobaron el examen final.
+          3). Listar los trainers que se encuentre trabajando en Campusland. 
+          4). Litar los campers que se encuentre en bajo rendimiento.
+          5). Listar los campers y trainers que se encuentren asociados a una ruta de entretenimiento.
+          6). Mostrar cuantos campers perdieron y aprobaron cada modulo. 
+    ----------------------------------------------------------------------------------------------------
+""")
+menuCoordinadorOpc6(0)
+    
