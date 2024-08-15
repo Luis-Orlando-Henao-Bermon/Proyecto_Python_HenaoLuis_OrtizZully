c051e8a4e4e68fb205a764357b1d836769b93e84
Luis_Henao <roronoazoro3704@gmail.com>
2024-08-15 11:02:47 -0500

    feat :sparkles: activation of luis' branch

--- Proyecto.py
+++ Proyecto.py
@@ -325,7 +325,7 @@ while x==0:#si no hay nadie con el usuario ingresado x seguira siendo 0 por lo t
                     campers=False
                     
 
-    for q in range(len(archivo["Trainers"])):#se usa un bucle for para recorra cada una  de las pociciones de los trainers
+    for q in range(len(archivo["Trainers"])):#se usa un bucle for para recorra cada una  de las pociciones de los trainers 
 
         if user == archivo["Trainers"][q]["user"]["login"]:#se miran todas las pociciones de los trainers y si el usuario (login) alguno de ellos coinciden con el ingresado x pasa a valer 1 y se rompe el bucle de while
             system("clear")
